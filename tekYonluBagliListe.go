75478a66de246a269e2653d66959793696f464c8
adem522 <adem_sahin12@hotmail.com>
2020-09-14 05:34:00 +0300

    Update tekYonluBagliListe.go
    source : https://medium.com/backendarmy/linked-lists-in-go-f7a7b27a03b9

--- tekYonluBagliListe.go
+++ tekYonluBagliListe.go
@@ -37,7 +37,7 @@ func (c *cins) turSil(gelenAd string) error {
 	gecici := c.turler
 	gecici2 := c.turler
 	if gecici == nil {
-		fmt.Println("ben aslinda yoğum")
+		fmt.Println("ben aslinda yogum")
 	} else {
 		if gecici.ad == gelenAd {
 			c.turler = c.turler.sonraki
